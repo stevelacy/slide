7464e73923fc8a41b0f303443e58bcaae4563d26
Steve Lacy <me@slacy.me>
2014-07-29 14:17:03 -0700

    base

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,23 @@ $(document).ready(function(){
   $('.message').html(process.version);
   console.log(path);
   $.adaptiveBackground.run();
+
+  var images = [];
+
+  $(".file").dropzone({
+    url: "#",
+    maxFilesize: 6,
+    dictResponseError: '',
+    accept: function(file, done) {
+      console.log(file.path);
+      images.push(file.path);
+      done("nope");
+    }
+  });
+
+  $('.go').click(function(){
+    $('.message').html(images);
+    console.log(images);
+  });
+
 });
