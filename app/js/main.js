cd0794a850438501543f096619f6d3d75b936e8d
Steve Lacy <me@slacy.me>
2014-07-29 15:10:14 -0700

    begin styling

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
+  var fs = require('fs');
   var path = require('path');
-  $('.message').html(process.version);
-  console.log(path);
+
   $.adaptiveBackground.run();
 
   var images = [];
@@ -10,16 +10,25 @@ $(document).ready(function(){
     url: "#",
     maxFilesize: 6,
     dictResponseError: '',
-    accept: function(file, done) {
+    accept: function(file, done){
       console.log(file.path);
       images.push(file.path);
       done("nope");
+    },
+    init: function(){
+      this.on('addedfile', function(file){
+        console.log(this);
+        $('.dz .message').fadeOut();
+      });
     }
   });
 
   $('.go').click(function(){
     $('.message').html(images);
     console.log(images);
+    images.forEach(function(v, k){
+      $('.images').append('<img src="' +v+ '" class="image">');
+    });
   });
 
 });
