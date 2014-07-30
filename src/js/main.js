67c0f5ba3daf33493a76a1a9fb61f66098ec28f8
Steve Lacy <me@slacy.me>
2014-07-30 09:43:03 -0700

    image slide :)

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,11 @@ $(document).ready(function(){
   var fs = require('fs');
   var path = require('path');
 
-  $.adaptiveBackground.run();
-
   var images = [];
 
   $(".file").dropzone({
     url: "#",
-    maxFilesize: 6,
+    maxFilesize: 3,
     dictResponseError: '',
     previewContainer: '.file .previews',
     accept: function(file, done){
@@ -24,11 +22,27 @@ $(document).ready(function(){
     }
   });
 
+  var slide = function(){
+    $.each(images, function(k, v){
+      var el = $('.images');
+      var pre = $('.pre-img');
+      setTimeout(function(){
+        if (el.is(':visible')){
+          el.fadeOut(500);
+        }
+        pre.html('<img src="' +v+ '" class="image" data-adaptive-background="1">');
+        $.adaptiveBackground.run();
+        pre.on('ab-color-found', function(e, payload){
+          el.html('<img src="' +v+ '" class="image">').css({
+            background: payload.color
+          }).fadeIn(500);
+        });
+      }, 4000 * k);
+    });
+  };
+
   $('.go').click(function(){
     $('.shadow').fadeOut();
-    images.forEach(function(v, k){
-      $('.images').append('<img src="' +v+ '" class="image">');
-    });
+    slide();
   });
-
 });
